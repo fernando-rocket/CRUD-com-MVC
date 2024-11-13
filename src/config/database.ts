f84cd3d5bc054387a26132453de9705bc8aa0633
Fernando <fernandinholindo350@gmail.com>
2024-11-13 09:19:38 -0300

    Add files via upload

---
+++ src/config/database.ts
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+//CARREGAR AS VARIAVEIS DE AMBIENTE .ENV
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port:3302,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+export {pool};
